7eabb2738bbff475df8d6d6333b9259bee3d3eee
VadimMonroe <propoved@mail.ru>
2022-03-23 20:50:22 +0300

    corrected errors & add new camera

--- cameras.py
+++ cameras.py
@@ -1,7 +1,8 @@
+import sys
+
 from settings import *
 import cv2
 from ffpyplayer.player import MediaPlayer
-import sys
 
 """
 system arguments variables:
@@ -11,17 +12,19 @@ examples:
 python cameras.py rec
 python cameras.py show
 """
-command = sys.argv[1]
+command = 'show' if len(sys.argv) == 1 else sys.argv[1]
 
 
 def show_must_go_on() -> None:
-    cameras = [f'rtsp://{ip}:554/user={login}&password={password}&channel={_}&stream=0.sdp?real_stream--rtp-caching=100'
-               for _ in range(1, 5)]
+    cameras = [f'rtsp://{ip_cam_1}:554/user={login}&password={password}&channel={_}' \
+               f'&stream=0.sdp?real_stream--rtp-caching=100' for _ in range(1, 5)]
+    cameras.append(f'rtsp://{ip_cam_2}:554/user={login}&password={password}&channel=1&stream=100.sdp')
+
     list_cam = [cv2.VideoCapture(_) for _ in cameras]
 
     """Настройки записи в файлы, fps=200 для быстрого просмотра и сокращения памяти"""
     output = [cv2.VideoWriter(f'archive/video_{i}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 200, (360, 240)) for i in
-              range(1, 5)]
+              range(1, 6)]
 
     print('watch')
     file_count = 0
@@ -32,12 +35,14 @@ def show_must_go_on() -> None:
         _, img22 = list_cam[1].read()
         _, img33 = list_cam[2].read()
         _, img44 = list_cam[3].read()
+        _, img55 = list_cam[4].read()
 
         try:
             img1 = cv2.resize(img11, (360, 240))
             img2 = cv2.resize(img22, (360, 240))
             img3 = cv2.resize(img33, (360, 240))
             img4 = cv2.resize(img44, (360, 240))
+            img5 = cv2.resize(img55, (360, 240))
 
             audio_frame, val = player.get_frame()
 
@@ -47,9 +52,10 @@ def show_must_go_on() -> None:
                 cv2.imshow("Gates", img2)
                 cv2.imshow("Car", img3)
                 cv2.imshow("Balcony", img4)
+                cv2.imshow("GuestRoom", img5)
 
                 if val != 'eof' and audio_frame is not None:
-                    img, t = audio_frame
+                    img342, t = audio_frame
 
             """Запись в файлы, включаем когда надо"""
             if command == 'rec':
@@ -57,6 +63,7 @@ def show_must_go_on() -> None:
                 output[1].write(img2)
                 output[2].write(img3)
                 output[3].write(img4)
+                output[4].write(img5)
         except Exception as E1:
             print('E1', E1)
             break
